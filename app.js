daa5f22bd0350601516987fa7cb9981aafcde1c1
Björn Schmidtke <bs@pa1nd.de>
2017-03-21 23:35:37 +0100

    fix app.js

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-    var express = require('express')
-    var app = express();
-    var jade = require('jade');
+var express = require('express')
+var app = express();
+var jade = require('jade');
 
-    //var request = require('request');
-    app.use('/js', express.static('js'))
-    app.use('/css', express.static('css'))
-    app.use('/img', express.static('img'))
+//var request = require('request');
+app.use('/js', express.static('js'))
+app.use('/css', express.static('css'))
+app.use('/img', express.static('img'))
 
 
-app.get('/', function (req, res, next) {
+app.get('/:id', function (req, res, next) {
     var file = 'index.jade';
-    var html = jade.renderFile(req.query.file||file);
+    var html = jade.renderFile(req.param.id||file);
     res.setHeader('Content-Type', 'text/html;charset=utf-8');
     res.end(html);
 });
 
 
 
-app.listen(process.env.PORT || 99, function () {
-    console.log('Listening on http://localhost:' + (process.env.PORT || 99))
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
 })
