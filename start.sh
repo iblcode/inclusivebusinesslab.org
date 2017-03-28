c93e59b4c29282e7400b3eb9b6cf0707b3bf6be2
Björn Schmidtke <bs@pa1nd.de>
2017-03-28 12:34:59 +0200

    fix

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ penguin run \
   --middleware [ \
     penguin-passwordless \
       --cookie [ --secret foobar ] \
-      --failure-redirect /login \
-      --sent-redirect /success \
+      --failure-redirect /en/login \
+      --sent-redirect /en/success \
       --pg-url "$DATABASE_URL" \
       --redis-url "$REDIS_URL" \
       --delivery [ \
