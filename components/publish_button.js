d52561c43f64487c74545c8abdcc0fe653becb72
Björn Schmidtke <bs@pa1nd.de>
2017-03-30 22:20:59 +0200

    implement penguin. add components

---
+++ components/publish_button.js
@@ -0,0 +1,10 @@
+export function mount(ctx, props, el) {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    ctx.publish()
+  })
+}
+
+export function render(ctx, props) {
+  return {replace: ''}
+}
