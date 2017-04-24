df14f2bfc7003f9ad746a0153a4759ea68ae6e98
Björn Schmidtke <bs@pa1nd.de>
2017-04-24 05:04:38 +0200

    implement links

--- pug-live-transform.js
+++ pug-live-transform.js
@@ -9,8 +9,7 @@ module.exports = () => (
   {
     transform (source, id) {
       const p = relative(process.cwd(), id)
-      if (!minimatch(p, 'components/*.pug')) return
-      if (minimatch(p, 'components/*.live.pug')) return
+      if (!minimatch(p, 'components/*.live.pug')) return
       const pugFn = pug.compileClient(source)
       const code =
         `${pugFn}
@@ -19,7 +18,7 @@ export function mount (ctx, props, el) {
     const html = render(ctx, props)
     if (html !== el.innerHTML) el.innerHTML = html
   }
-
+  if (process.env.PENGUIN_ENV === 'development') ctx.store.subscribe(update)
   update()
 }
 export function render ({ store, language }, props) {
