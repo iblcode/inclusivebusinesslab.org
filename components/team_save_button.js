135f63a849549ce0ef1e5c16c7b5bd253334e806
Björn Schmidtke <bs@pa1nd.de>
2017-05-28 21:01:43 +0200

    refactoring

--- components/team_save_button.js
+++ components/team_save_button.js
@@ -1,47 +1,37 @@
 import find from 'lodash/fp/find'
 import { update } from 'penguin.js'
 
-export function mount (ctx, props, el) {
-  el.addEventListener("click", e => {
+export function mount(ctx, props, el) {
+  el.addEventListener('click', e => {
     e.preventDefault()
-    saveGloabl(ctx, props, el)
+    saveGlobal(ctx, props, el)
     ctx.save()
   })
 }
 
-function saveGloabl (ctx, props, el) {
-
-  const id = window.location.pathname.split('/')[3]
-
+function saveGlobal(ctx, props, el) {
+  const { keys, field } = props
   const { store } = ctx
   const { fields } = store.getState()
-
-  const teamMembers = fields.teamMembers || []
-
-  const image = fields.image
-  const name = fields.name
-  const content = fields.content
-  const url = fields.url
-  const type = fields.type
-
-  const teamMember = find({ id }, teamMembers)
-  const newTeamMembers = (
-    teamMember == null
-      ? [...teamMembers, { id, image, name, content, url, type }]
-      : (
-        teamMembers.map(
-          m => (
-            m.id === id
-              ? { id, image, name, content, url, type }
-              : m
-          )
-        )
-      )
+  const id = window.location.pathname.split('/')[3]
+  const current = keys.reduce(
+    (current, key) =>
+      Object.assign({}, current, {
+        [key]: fields[key]
+      }),
+    { id }
   )
-
-  store.dispatch(update({ teamMembers: newTeamMembers }))
+  const globalElements = fields[field] || []
+  const element = find({ id }, globalElements)
+  store.dispatch(
+    update({
+      [field]: element == null
+        ? [...globalElements, current]
+        : globalElements.map(m => (m.id === id ? current : m))
+    })
+  )
 }
 
-export function render (ctx, props) {
+export function render(ctx, props) {
   return { replace: '' }
 }
