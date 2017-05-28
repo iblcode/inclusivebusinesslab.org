fa42a3d9590d478a3ed031ca588c3519b2500c96
Björn Schmidtke <bs@pa1nd.de>
2017-05-28 23:11:31 +0200

    add form query

--- components/form.js
+++ components/form.js
@@ -1,55 +1,56 @@
-import {
-  mount as mountInplace,
-  render as renderInplace
-} from 'penguin-inplace'
+import { mount as mountInplace, render as renderInplace } from 'penguin-inplace'
+
+import URI from 'urijs'
+// const URI = require('urijs')
 
 export function mount(ctx, props, el) {
-  el.addEventListener('click', function (e) {
+  el.addEventListener('click', function(e) {
     e.preventDefault()
 
     var city = document.getElementById('city').value
     var school = document.getElementById('school').value
 
-    if (city != '' && school != '' ) {
+    if (city != '' && school != '') {
       el.disabled = true
       el.style.display = 'none'
       document.getElementById('spinner').style.display = 'block'
 
       var request = new XMLHttpRequest()
-      request.open("GET", "https://penguin.inclusivebusinesslab.org/form")
+      request.open('GET', 'https://penguin.inclusivebusinesslab.org/form')
 
       request.onload = function() {
         success(request.responseText, city, school)
       }
-      
+
       request.onerror = function() {
-        document.getElementById("action").innerHTML = "Error! Contact us."
+        document.getElementById('action').innerHTML = 'Error! Contact us.'
         console.warn(request.statusText, request.responseText)
       }
 
       request.send()
-    }
-    else {
-      var elem = document.getElementById("action")
+    } else {
+      var elem = document.getElementById('action')
       var p = document.createElement('p')
-      p.innerHTML = "Error! Select city and school."
+      p.innerHTML = 'Error! Select city and school.'
       elem.insertBefore(p, elem.childNodes[0])
     }
-
-
-
   })
   mountInplace(ctx, props, el)
-
 }
 
 export function render(ctx, props) {
-
   return renderInplace(ctx, props)
-
 }
 
-function success (answer, city, school) {
-  var id = JSON.parse(answer).id
-  window.location.href = `https://iblfeedback.typeform.com/to/gVJAzd?id=${city+school+id}`
+function success(answer, city, school) {
+  let id = JSON.parse(answer).id
+
+  let url = URI(window.location.href)
+  let form = URI(
+    url.search(true).form || 'https://iblfeedback.typeform.com/to/gVJAzd'
+  )
+  form.protocol('https')
+  form.search({ id: `${city + school + id}` })
+
+  window.location.href = form.toString()
 }
