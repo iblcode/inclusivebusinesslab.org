7215b8ce9de28a57078341eb7bd96354d6fd3649
Björn Schmidtke <bs@pa1nd.de>
2017-05-28 21:00:37 +0200

    change description

--- components/team_add_button.js
+++ components/team_add_button.js
@@ -1,16 +1,16 @@
 import slugify from 'slugify'
 
-
-export function mount (ctx, props, el) {
-  function relocate (slug) {
+export function mount(ctx, props, el) {
+  function relocate(slug) {
     window.location.href = `/${ctx.language}/team/${slug}`
   }
 
   el.addEventListener('click', () => {
-    const id = prompt("Next-Gen Leader's Name?")
+    const id = prompt("Person's Name? (will become ID)")
     relocate(slugify(id))
   })
-
 }
 
-export function render () { return { replace: '' } }
+export function render() {
+  return { replace: '' }
+}
