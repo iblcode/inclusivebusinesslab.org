356f2bce860f3ce9037d779858ba55a5cb809f0b
Björn Schmidtke <bs@pa1nd.de>
2017-06-17 10:31:31 +0200

    add sorting

--- components/publish_button.js
+++ components/publish_button.js
@@ -1,8 +1,10 @@
 export function mount(ctx, props, el) {
-  el.addEventListener("click", e => {
+  el.addEventListener('click', e => {
     e.preventDefault()
     ctx.publish()
   })
 }
 
-export function render(ctx, props) { return {replace: ''} }
+export function render(ctx, props) {
+  return { replace: '' }
+}
