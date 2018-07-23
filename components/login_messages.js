502a4c1fdb64a8bf17bb69f7f34e68abb262c3c0
Björn Schmidtke <bs@pa1nd.de>
2018-07-23 05:06:53 +0400

    fix login. why did it ever work?

--- components/login_messages.js
+++ components/login_messages.js
@@ -1,11 +1,12 @@
-export function mount ( ctx, props, el ) {
-  if ( process.env.PENGUIN_ENV === 'production' ) return
+export function mount(ctx, props, el) {
+  if (process.env.PENGUIN_ENV === 'production') return
 
-  if ( !window.location.search.slice( 1 ).match( /success=1/ ) ) return
+  if (!window.location.search.slice(1).match(/success=1/)) return
 
-  el.querySelector( '.success' ).classList.remove( 'hidden' )
-  el.querySelector( '.login' ).classList.add( 'hidden' )
-
+  el.querySelector('.success').classList.remove('hidden')
+  el.querySelector('.login').classList.add('hidden')
 }
 
-export function render () { return { replace: '' } }
+export function render() {
+  return { attrs: {} }
+}
